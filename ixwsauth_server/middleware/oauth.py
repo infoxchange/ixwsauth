c5d1b9b32824755a78ed5c3800267779ed5787bd
Alexey Kotlyarov <a@koterpillar.com>
2015-05-08 21:55:09 +1000

    Fix PEP8

--- ixwsauth_server/middleware/oauth.py
+++ ixwsauth_server/middleware/oauth.py
@@ -4,9 +4,9 @@ Module for backward compatibility with existing imports.
 
 from warnings import warn
 
+# pylint:disable=unused-wildcard-import
+from ixwsauth_server.middleware import *
+
 warn("ixwsauth_server.middleware.oauth is deprecated; "
      "use ixwsauth_server.middleware directly.",
      DeprecationWarning)
-
-# pylint:disable=unused-wildcard-import
-from ixwsauth_server.middleware import *
